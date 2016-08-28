bb33de1d1ab6fa4ae1b71d58edcfe6b239d4ba7b
joy <joy.jing1@gmail.com>
2016-08-28 12:02:34 -0700

    Create minimal binary search tree from a sorted array

---
+++ js_practice/min_bin_search_tree.js
@@ -0,0 +1,18 @@
+// Given a sorted (increasing order) array with unique integer elements,
+// write an algorithm to create a binary search tree with minimal height.
+
+
+// O(n) time, O(n) space
+
+function minBinarySearchTree(arr) {
+  if (arr.length <= 0) { return undefined; }
+  const rootIdx = Math.floor(arr.length / 2);
+
+  const root = new Node(arr[rootIdx]);
+  const left = minBinarySearchTree(arr.slice(0, rootIdx));
+  const right = minBinarySearchTree(arr.slice(rootIdx + 1));
+
+  root.left = left;
+  root.right = right;
+  return root;
+}
