a86c61dcc79bdbec31861b9cd3eabb2badb00642
joy <joy.jing1@gmail.com>
2016-09-07 17:34:47 -0700

    Create Lyft new driver form

--- frontend_practice/debounce.js
+++ frontend_practice/debounce.js
@@ -1,1 +1,35 @@
-// NEED TO WRITE
+
+function myFunc() {
+  console.log(new Date());
+}
+
+function debounceFull(fun, wait, immediate) {
+  let prevTime;
+  return function() {
+    let context = this, args = arguments;
+    let later = function() {
+      timeout = null;
+      if (!immediate) fun.apply(context, args);
+    };
+    let callNow = immediate && !prevTime;
+    clearTimeout(prevTime);
+    prevTime = setTimeout(later, wait);
+    if (callNow) fun.apply(context, args);
+  };
+}
+
+function debounce(fun) {
+  let prevTime;
+
+  return function() {
+    clearTimeout(prevTime);
+    prevTime = setTimeout(fun, 1000);
+  };
+}
+
+let debFunc = debounce(myFunc, 1000);
+debFunc();
+debFunc();
+debFunc();
+debFunc();
+debFunc();
