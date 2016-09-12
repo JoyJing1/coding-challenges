e0e470832d020bd859a0acd5c8e215da1cb2ddcb
joy <joy.jing1@gmail.com>
2016-09-11 22:22:33 -0700

    Partition set - determine whether an array of integers can be split into two distinct sets with equal sums

---
+++ algorithms/partition_set.js
@@ -0,0 +1,26 @@
+// Partition problem is to determine whether a given array of integerss
+// can be partitioned into two subsets such that
+// the sum of elements in both subsets is same.
+
+function canPartition(arr) {
+  let sum = arr.reduce( (all, el) => all + el );
+  if (sum % 2 === 1) { return false; }
+
+  return isSubsetSum(arr, arr[arr.length - 1], sum/2);
+}
+
+function isSubsetSum(arr, n, sum) {
+  if (sum === 0) { return true; }
+  if (sum === n) { return true; }
+  if (arr.length <= 0) { return false; }
+
+  let include = isSubsetSum(arr.slice(0, arr.length-1), arr[arr.length-2], sum - n);
+  let exclude = isSubsetSum(arr.slice(0, arr.length-1), arr[arr.length-2], sum);
+
+  return include || exclude;
+}
+
+console.log(canPartition([1,5,11,5])); // true
+console.log(canPartition([1,5,11,6])); // false
+console.log(canPartition([1,5,21,16,1])); // truee
+console.log(canPartition([1,5,21,6])); // false
